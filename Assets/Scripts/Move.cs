763b3865788a9e9111128bc72dab107126d5c1ff
Criquette <38935251+CriquetteRockwell@users.noreply.github.com>
2019-12-02 11:54:45 +0100

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        float x = Random.Range(-25, 26);
+        float y = 0;
+        float z = Random.Range(-25, 26);
+        Vector3 pos = new Vector3(x, y, z);
+        agent.destination = pos; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
